702e8e3dc27b724b908796f32ec907ee009053ca
Shubham007-tech <91582453+Shubham007-tech@users.noreply.github.com>
2022-01-21 14:47:31 +0530

    Add to Cart function Update

--- Productpage/vegApp.js
+++ Productpage/vegApp.js
@@ -76,10 +76,16 @@ function viewProducts(d)
      let price = document.createElement('p');
      price.textContent= "₹"+el.price;
      price.setAttribute("class" , "card_price")
+     price.onclick = function () {
+        referData(el)
+    }
  
      let title = document.createElement('h3');
      title.textContent= el.name ;
      title.setAttribute("class" , "card_title")
+     title.onclick = function () {
+        referData(el)
+    }
 
      let discount= document.createElement('p');
      discount.textContent= el.discount+ " Off";
@@ -108,7 +114,7 @@ function viewProducts(d)
      img.src= el.image_links;
      img.setAttribute("class" , "card_img")
 
-     div.onclick = function () {
+     img.onclick = function () {
         referData(el)
     }
     
