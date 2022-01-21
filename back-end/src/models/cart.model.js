b6ac9b119b83441ff5c7c4f34538fb03af5d8518
akasaurabhverma <thisissaurabhverma@gmail.com>
2022-01-21 12:44:40 +0530

    cart controller

--- back-end/src/models/cart.model.js
+++ back-end/src/models/cart.model.js
@@ -2,7 +2,7 @@ const { model, Schema } = require('mongoose');
 
 const cartSchema = Schema({
     product_id: { type: Schema.Types.ObjectId, ref: 'product', required: true },
-    quantity: { type: Number, required: true, default: 0 }
+    quantity: { type: Number, required: true, default: 1 }
 }, {
     versionKey: false,
     timestamps: true
